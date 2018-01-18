e414aa8a21318dbdf58d736c4adbc4e3d852c609
Rafael Garcia <rafuk89@gmail.com>
2018-01-18 04:55:30 +0100

    added Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+  
+    watch: {
+      sass: {
+        files: '**/*.scss', 
+        tasks: ['css']
+      }
+    },
+
+    sass: {
+      dev: {
+        files: {
+          'css/main.css' : 'scss/main.scss'
+        }
+      }
+    },
+
+    cssmin: {
+      build: {
+        src: 'css/main.css',
+        dest: 'css/main.min.css'
+      }    
+    },
+
+    concat: {
+      options: {
+        separator: '\n/*next file*/\n\n'
+      },
+      dist: {
+        src: ['scripts/*.js'],
+        dest: 'scripts/built.js'
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'scripts/built.min.js': ['scripts/built.js']
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify']);
+
+};
