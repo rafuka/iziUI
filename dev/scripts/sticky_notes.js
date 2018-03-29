be909315c5e8507cdbaaeb902b5877b33c2fd32c
Rafael Garcia <rafuk89@gmail.com>
2018-03-29 15:34:30 +0200

    added pin button and its functionality to sticky notes

--- dev/scripts/sticky_notes.js
+++ dev/scripts/sticky_notes.js
@@ -5,52 +5,6 @@
 
 	var createStickyNoteElement = document.getElementsByClassName('izi-sticky-pack__create')[0];
 
-	function createStickyNote(posX, posY) {
-		var newNote = {
-			isFixed: true,
-			left: posX,
-			top: posY,
-			content: 'Write something!'
-		};
-
-		var noteText = document.createTextNode(newNote.content);
-		var newNoteElement = document.createElement('div');
-
-		var noteEditBtn = document.createElement('button');
-
-		noteEditBtn.classList.add('izi-sticky-pack__note-edit');
-
-		var noteTextP = document.createElement('p');
-
-		noteTextP.appendChild(noteText);
-		noteTextP.setAttribute('contenteditable', 'true');
-		noteTextP.classList.add('izi-sticky-pack__note-text');
-		
-		newNoteElement.classList.add('izi-sticky-pack__note');
-		newNoteElement.classList.add('dragging');
-		
-		newNoteElement.style.top = newNote.top;
-		newNoteElement.style.left = newNote.left;
-
-		newNoteElement.appendChild(noteTextP);
-		newNoteElement.appendChild(noteEditBtn);
-
-		activeStickyNotes.push(newNoteElement);
-
-		return newNoteElement;
-	}
-
-
-	// TODO: Listen on clicks on the createStickyNoteElement and create a new sticky note
-	createStickyNoteElement.addEventListener('click', function(e) {
-
-		var newNoteElement = createStickyNote('100px', '100px');
-		
-		document.body.appendChild(newNoteElement);
-
-
-	});
-
 	function handleDrag(event) {
 
 		var newX = this.startX - event.clientX;
@@ -61,6 +15,116 @@
 		this.startY = event.clientY;
 		
 	}
+
+	function createStickyNote(posX, posY) {
+
+		var newNote = {
+			isFixed: true,
+			left: posX,
+			top: posY,
+			content: 'Click the edit button to write something.'
+		};
+
+		var noteText = document.createTextNode(newNote.content);
+		var newNoteElement = document.createElement('div');
+		var notePinBtn = document.createElement('button');
+		var noteEditBtn = document.createElement('button');
+		var noteTextP = document.createElement('p');
+
+		noteTextP.appendChild(noteText);
+
+		noteTextP.classList.add('izi-sticky-pack__note-text');
+		noteTextP.setAttribute('contenteditable', 'false');
+
+		notePinBtn.classList.add('izi-sticky-pack__note-pin');
+
+		notePinBtn.addEventListener('click', function() {
+
+			var noteOffset = newNoteElement.getBoundingClientRect();
+
+			if (newNote.isFixed) {
+				newNoteElement.style.position = 'absolute';
+				newNoteElement.style.top = (window.scrollY + noteOffset.top) + 'px';
+				newNote.isFixed = false;
+				newNote.posX = newNoteElement.style.left;
+				newNote.posY = newNoteElement.style.top;
+			}
+			else {
+				newNoteElement.style.position = 'fixed';
+				newNoteElement.style.top = noteOffset.top + 'px';
+				newNote.isFixed = true;
+				newNote.posX = newNoteElement.style.left;
+				newNote.posY = newNoteElement.style.top;
+			}
+		});
+
+		noteEditBtn.classList.add('izi-sticky-pack__note-edit');
+
+		noteEditBtn.addEventListener('click', function() {
+			if (noteTextP.getAttribute('contenteditable') === 'false') {
+				noteTextP.setAttribute('contenteditable', 'true');
+				noteTextP.focus();
+			}
+			else {
+				noteTextP.setAttribute('contenteditable', 'false');
+			}	
+		});
+
+		
+		
+		newNoteElement.classList.add('izi-sticky-pack__note');
+		newNoteElement.classList.add('dragging');
+
+		switch (Math.floor((Math.random() * 3) + 1)) {
+			case 1:
+				newNoteElement.classList.add('izi-sticky-pack__note--green');
+				break;
+			case 2: 
+				newNoteElement.classList.add('izi-sticky-pack__note--yellow');
+				break;
+			case 3:
+				newNoteElement.classList.add('izi-sticky-pack__note--pink');
+				break;
+		}
+		
+		newNoteElement.style.top = newNote.top;
+		newNoteElement.style.left = newNote.left;
+
+		newNoteElement.appendChild(noteTextP);
+		newNoteElement.appendChild(noteEditBtn);
+		newNoteElement.appendChild(notePinBtn);
+
+		newNoteElement.addEventListener('mousedown', function(e) {
+
+			console.log('testing');
+			this.startX = e.clientX;
+			this.startY = e.clientY;
+			this.addEventListener('mousemove', handleDrag, false);
+
+		}, false);
+
+		newNoteElement.addEventListener('mouseup', function(e) {
+				
+			this.removeEventListener('mousemove', handleDrag, false);
+
+		}, false);
+
+		activeStickyNotes.push(newNoteElement);
+
+		return newNoteElement;
+	}
+
+	createStickyNoteElement.addEventListener('click', function(e) {
+
+		var newNoteElement = createStickyNote('100px', '100px');
+		var transformDegree = Math.floor((Math.random() * 12) - 6);
+		newNoteElement.style.transform = 'rotate(' + transformDegree + 'deg)';
+		document.body.appendChild(newNoteElement);
+
+
+	});
+
+	
 
 	// Check if there is empty space between the border of the draggable area and its parent.
 	// In that case, move the area's border back to its initial position.
